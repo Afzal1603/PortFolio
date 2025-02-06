4301866805586f603ddfd8ff263ca9afa7b60933
Mohd Afzal Ansari <afzalansaria660@gmail.com>
2025-02-07 01:33:48 +0530

    images updated

--- src/data/data.js
+++ src/data/data.js
@@ -3,21 +3,21 @@ const data = [
     name: "Chat Application",
     content:
       "A MERN chat application enables real-time messaging using MongoDB, Express, React, and Node.js. It features user authentication, one-to-one and group chats. ",
-    img: "https://res-console.cloudinary.com/dpz8bmqix/thumbnails/v1/image/upload/v1738870952/V2hhdHNBcHBfSW1hZ2VfMjAyNC0xMi0xMl9hdF8yMi41My41Ml8xODUzNmRhMV96cGl4emY=/drilldown",
+    img: "https://res.cloudinary.com/dpz8bmqix/image/upload/v1738870952/WhatsApp_Image_2024-12-12_at_22.53.52_18536da1_zpixzf.jpg",
     url: "",
   },
   {
     name: "Advance Authentication",
     content:
       "An advanced authentication app using MERN features JWT-based authentication, role-based access control, and email verification. ",
-    img: "https://res-console.cloudinary.com/dpz8bmqix/thumbnails/v1/image/upload/v1738870976/U2NyZWVuc2hvdF8xMTNfb3dtdWg1/drilldown",
+    img: "https://res.cloudinary.com/dpz8bmqix/image/upload/v1738870976/Screenshot_113_owmuh5.png",
     url: "",
   },
   {
     name: "Image Gallery",
     content:
       "A web app that uses the Pixabay API to fetch and display images in a gallery. It features search functionality, pagination, responsive design, and options to view or download images.",
-    img: "https://res-console.cloudinary.com/dpz8bmqix/thumbnails/v1/image/upload/v1738870976/U2NyZWVuc2hvdF8xMTRfZHJmcHNz/drilldown",
+    img: "https://res.cloudinary.com/dpz8bmqix/image/upload/v1738870976/Screenshot_114_drfpss.png",
     url: "https://imagegallery-ingenium.netlify.app/",
   },
 ];
